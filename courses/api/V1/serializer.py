f956168f95c442739eea23b424f988c6ef490799
django1401 <djangogroup1401@gmail.com>
2023-11-24 12:57:35 +0330

    create other method

--- courses/api/V1/serializer.py
+++ courses/api/V1/serializer.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
+from ...models import Course
 
 
-class CourseApiSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=200)
-    price = serializers.IntegerField()
+# class CourseApiSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=200)
+#     price = serializers.IntegerField()
+
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Course
+        fields = ["id", "title", "price", "content", "category", "teacher"]
+
+
+
+
+class CourseDetailSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Course
+        fields = ["title", "price", "content", "teacher", "category", "status"]
--- courses/api/V1/views.py
+++ courses/api/V1/views.py
@@ -1,20 +1,37 @@
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .serializer import CourseApiSerializer
+from .serializer import CourseSerializer, CourseDetailSerializer
 from ...models import Course
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 
 
 @api_view()
 def course_api_view(request):
     courses = Course.objects.filter(status=True)
-    courses_serilize = CourseApiSerializer(courses, many=True)
+    courses_serilize = CourseSerializer(courses, many=True)
     return Response(courses_serilize.data)
 
-@api_view()
+@api_view(["GET", "POST", "PUT", "DELETE"])
 def course_api_detail_view(request, pk):
     course = get_object_or_404(Course, id=pk)
-    course_serilize = CourseApiSerializer(course)
-    return Response(course_serilize.data)
+    if request.method == "GET":
+        course_serilize = CourseDetailSerializer(course)
+        return Response(course_serilize.data)
+    elif request.method == "POST":
+        course_serilize = CourseDetailSerializer(data=request.data)
+        if course_serilize.is_valid():
+            course_serilize.save()
+            return Response(course_serilize.data)
+        else:
+            return Response(course_serilize.errors)
+    elif request.method == "PUT":
+        course_serilize = CourseDetailSerializer(course, data=request.data)
+        course_serilize.is_valid(raise_exception=True)
+        course_serilize.save()
+        return Response(course_serilize.data)
+    elif request.method == "DELETE":
+        course.delete()
+        return Response("course deleted", status=status.HTTP_204_NO_CONTENT)
