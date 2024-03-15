3a4228ec5ef8b58862d9dbb82a8dcd5dda1950ad
django1401 <djangogroup1401@gmail.com>
2024-03-15 11:55:29 +0330

    create github actioin

--- courses/api/V1/tests.py
+++ courses/api/V1/tests.py
@@ -1,16 +1,26 @@
 import pytest
 from rest_framework.test import APIClient
+from accounts.models import CustomeUser
 
+
+@pytest.fixture
+def client_api():
+    client = APIClient()
+    return client
+
+@pytest.fixture
+def user_test():
+    user = CustomeUser.objects.create_user(username='test', password='H@midreza62', email='test@test.com')
+    return user
 
 
 
 @pytest.mark.django_db
 class TestCourses:
 
-    def test_view_api_course_list_view(self):
+    def test_view_api_course_list_view(self, client_api):
         url = "http://127.0.0.1:8000/courses/api/V1/courses/"
-        client = APIClient()
-        response = client.get(url)
+        response = client_api.get(url)
         assert response.status_code == 200
 
     def test_view_api_view_category(self):
@@ -28,10 +38,31 @@ class TestCourses:
         response = client.post(url, data=data)
         assert response.status_code == 401
 
-    def test_view_api_create_category_with_normal_user(self):
-        pass
+    def test_view_api_create_category_with_normal_user(self, user_test):
+        user = user_test
+        url = "http://127.0.0.1:8000/courses/api/V1/category/"
+        client = APIClient()
+        client.force_authenticate(user)
+        data = {
+            'name': 'test',
+        }
+        response = client.post(url, data)
+        assert response.status_code == 403
 
-    def test_view_api_create_category_with_superuser(self):
-        pass
+    def test_view_api_create_category_with_superuser(self ,user_test):
+        user = user_test
+        user.is_staff = True
+        user.is_active = True
+        user.is_superuser = True
+        user.is_verified = True
+        user.save()
+        url = "http://127.0.0.1:8000/courses/api/V1/category/"
+        client = APIClient()
+        client.force_authenticate(user)
+        data = {
+            'name': 'test',
+        }
+        response = client.post(url, data)
+        assert response.status_code == 201
 
 
